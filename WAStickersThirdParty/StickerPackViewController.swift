93b1b23b2d32a368e1ceb232431049d4af064b48
Joao Victor Flores <j199818@dac.unicamp.br>
2020-06-10 00:21:17 -0300

    refactory

--- WAStickersThirdParty/StickerPackViewController.swift
+++ WAStickersThirdParty/StickerPackViewController.swift
@@ -247,32 +247,14 @@ class StickerPackViewController: UIViewController, UICollectionViewDataSource, U
         
         if let img = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
         {
-            image = img
-            
-            var auxImg = img
-            if(img.size.width < img.size.height) {
-                auxImg = ImageFunctions().cropToBounds(image: img, width: Double(img.size.width), height: Double(img.size.width))
-            }
-            else if (img.size.width > img.size.height){
-                auxImg = ImageFunctions().cropToBounds(image: img, width: Double(img.size.height), height: Double(img.size.height))
-            }
-            
-            image = resizeImage(image: auxImg, targetSize: CGSize(width: 512, height: 512))
+            image = img.cropToSquare()
+            image = image.resizeImage(targetSize: CGSize(width: 512, height: 512))
         }
             
         else if let img = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         {
-            image = img
-            
-            var auxImg = img
-            if(img.size.width < img.size.height) {
-                auxImg = ImageFunctions().cropToBounds(image: img, width: Double(img.size.width), height: Double(img.size.width))
-            }
-            else if (img.size.width > img.size.height){
-                auxImg = ImageFunctions().cropToBounds(image: img, width: Double(img.size.height), height: Double(img.size.height))
-            }
-            
-            image = resizeImage(image: auxImg, targetSize: CGSize(width: 512, height: 512))
+            image = img.cropToSquare()
+            image = image.resizeImage(targetSize: CGSize(width: 512, height: 512))
         }
         
         picker.dismiss(animated: true,completion: nil)
@@ -290,7 +272,7 @@ class StickerPackViewController: UIViewController, UICollectionViewDataSource, U
             let sticker: Sticker = try Sticker(contentsOfFile: "placeholderGreen.png", emojis: nil)
             
             if let image = getSavedImage(named: "ProfileImg") {
-                let aux = image.resizeToApprox(sizeInMB: 0.5)
+                let aux = image.resizeToApprox(sizeInMB: 1)
                 let imageData = ImageData(data: aux, type: ImageDataExtension.png)
                 sticker.imageData = imageData
             }
@@ -301,33 +283,8 @@ class StickerPackViewController: UIViewController, UICollectionViewDataSource, U
         
         stickersCollectionView.reloadData()
     }
-
-    //    MARK: - Image Manipulator
-    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
-        let size = image.size
-
-        let widthRatio  = targetSize.width  / size.width
-        let heightRatio = targetSize.height / size.height
-
-        var newSize: CGSize
-        if(widthRatio > heightRatio) {
-            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
-        } else {
-            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
-        }
-
-        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
-
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
-        image.draw(in: rect)
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        return newImage!
-    }
     
     //    MARK: - Data Manager
-    
     //    Save
     func saveImage(image: UIImage) -> Bool {
         guard let data = image.jpegData(compressionQuality: 1) ?? image.pngData() else {
@@ -372,7 +329,7 @@ class StickerPackViewController: UIViewController, UICollectionViewDataSource, U
     }
 }
 
-// MARK: - Extension
+// MARK: - Extension Image Handle
 extension UIImage {
     func resizeToApprox(sizeInMB: Double, deltaInMB: Double = 0.2) -> Data {
         let allowedSizeInBytes = Int(sizeInMB * 1024 * 1024)
@@ -407,9 +364,80 @@ extension UIImage {
             }
              mid = (left + right) / 2.0
             newResImage = self.jpegData(compressionQuality: mid)
-
         }
 
         return self.jpegData(compressionQuality: 0.5)!
+    }
+    
+    func resizeImage(targetSize: CGSize) -> UIImage {
+        let size = self.size
+
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        self.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage!
+    }
+    
+    func cropToBounds(image: UIImage, width: Double, height: Double) -> UIImage {
+
+        let cgimage = image.cgImage!
+        let contextImage: UIImage = UIImage(cgImage: cgimage)
+        let contextSize: CGSize = contextImage.size
+        var posX: CGFloat = 0.0
+        var posY: CGFloat = 0.0
+        var cgwidth: CGFloat = CGFloat(width)
+        var cgheight: CGFloat = CGFloat(height)
+
+        // See what size is longer and create the center off of that
+        if contextSize.width > contextSize.height {
+            posX = ((contextSize.width - contextSize.height) / 2)
+            posY = 0
+            cgwidth = contextSize.height
+            cgheight = contextSize.height
+        } else {
+            posX = 0
+            posY = ((contextSize.height - contextSize.width) / 2)
+            cgwidth = contextSize.width
+            cgheight = contextSize.width
+        }
+
+        let rect: CGRect = CGRect(x: posX, y: posY, width: cgwidth, height: cgheight)
+
+        // Create bitmap image from context using the rect
+        let imageRef: CGImage = cgimage.cropping(to: rect)!
+
+        // Create a new image based on the imageRef and rotate back to the original orientation
+        let image: UIImage = UIImage(cgImage: imageRef, scale: image.scale, orientation: image.imageOrientation)
+        
+        return image
+    }
+    
+    func cropToSquare() -> UIImage {
+        var auxImg = self
+        if(self.size.width < self.size.height) {
+            auxImg = self.cropToBounds(image: self, width: Double(self.size.width), height: Double(self.size.width))
+        }
+        else if (self.size.width > self.size.height){
+            auxImg = self.cropToBounds(image: self, width: Double(self.size.height), height: Double(self.size.height))
+        }
+        
+        return auxImg
     }
 }

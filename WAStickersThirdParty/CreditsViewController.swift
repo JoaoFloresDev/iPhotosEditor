a03f9a7ca74d82529582e425c120725e66b0c91f
Joao Victor Flores <j199818@dac.unicamp.br>
2020-06-11 20:50:24 -0300

    creditsDone

---
+++ WAStickersThirdParty/CreditsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CreditsViewController.swift
+//  WAStickersThirdParty
+//
+//  Created by Joao Flores on 11/06/20.
+//  Copyright © 2020 WhatsApp. All rights reserved.
+//
+
+import UIKit
+
+class CreditsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeView(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

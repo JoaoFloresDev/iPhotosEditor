32afd2abfbd56c748a0a0c276518d599296f88b3
Joao Victor Flores <j199818@dac.unicamp.br>
2021-04-18 18:54:00 -0300

    up

---
+++ CollageMaker/Controllers/HomeVC.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Photo Collage Maker
+//
+//  Created by Grapes Infosoft on 14/09/19.
+//  Copyright © 2019 Grapes Infosoft. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController{
+
+    //MARK:- outlets
+    @IBOutlet weak var btnMyAlbums: UIButton!
+    @IBOutlet weak var btnGrid: UIButton!
+    @IBOutlet weak var btnEdit: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MARK:- Button Action Zone
+    @IBAction func btnGridAction(_ sender: Any) {
+        let obj : LoadShapesVC = self.storyboard?.instantiateViewController(withIdentifier: "LoadShapesVC") as! LoadShapesVC
+        self.navigationController?.pushViewController(obj, animated: true)
+    }
+  
+    @IBAction func btnEditAction(_ sender: Any) {
+        let obj = self.storyboard!.instantiateViewController(withIdentifier: "PresentPhotoVC") as! PresentPhotoVC
+        obj.objSelectiontype = 2
+        let navController = UINavigationController(rootViewController: obj)
+        navController.navigationBar.isHidden = true
+        navController.modalPresentationStyle = .overCurrentContext
+        self.present(navController, animated:true, completion: nil)
+    }
+    @IBAction func btnMyAlbumsAction(_ sender: Any) {
+        let obj : MyAlbumVC = self.storyboard?.instantiateViewController(withIdentifier: "MyAlbumVC") as! MyAlbumVC
+        self.navigationController?.pushViewController(obj, animated: true)
+    }
+}
+

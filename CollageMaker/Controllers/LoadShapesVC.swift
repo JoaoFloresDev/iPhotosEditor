ad1575ff9aee9cff8b7a923b98b07656bde40da8
Joao Victor Flores <j199818@dac.unicamp.br>
2021-04-18 23:57:29 -0300

    up

--- CollageMaker/Controllers/LoadShapesVC.swift
+++ CollageMaker/Controllers/LoadShapesVC.swift
@@ -14,6 +14,7 @@ class LoadShapesVC: UIViewController,UICollectionViewDelegate,UICollectionViewDa
     
     @IBOutlet weak var btnBack: UIButton!
     @IBOutlet weak var ShapeCV: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         ShapeCV.delegate = self
